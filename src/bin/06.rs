0d8e40ee6ed5dc0725f991307866ec505f80c456
Douglas Rocha <79188161+dougrocha@users.noreply.github.com>
2024-12-10 14:47:22 -0700

    completed day 06 part 2

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -1,7 +1,8 @@
 advent_of_code_2024::solution!(6);
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Default, Debug, Clone, Copy, PartialEq, Eq)]
 enum Direction {
+    #[default]
     Up,
     Down,
     Left,
@@ -15,6 +16,15 @@ impl Direction {
             Direction::Down => (1, 0),
             Direction::Left => (0, -1),
             Direction::Right => (0, 1),
+        }
+    }
+
+    fn turn_right(self) -> Direction {
+        match self {
+            Direction::Up => Direction::Right,
+            Direction::Down => Direction::Left,
+            Direction::Left => Direction::Up,
+            Direction::Right => Direction::Down,
         }
     }
 }
@@ -71,22 +81,105 @@ impl Map {
 
                 if let Some(to_check) = self.coords_to_index(check_x, check_y) {
                     if self.arr[to_check] == '#' {
-                        cur_direction = match cur_direction {
-                            Direction::Up => Direction::Right,
-                            Direction::Down => Direction::Left,
-                            Direction::Left => Direction::Up,
-                            Direction::Right => Direction::Down,
-                        };
+                        cur_direction = cur_direction.turn_right();
+                    } else {
+                        x = check_x;
+                        y = check_y;
                     }
-
-                    let offset = &cur_direction.to_offset();
-                    x += offset.0;
-                    y += offset.1;
                 } else {
                     break;
                 }
             }
         }
+    }
+
+    fn parse_cycle_paths(&mut self, start: (usize, usize)) -> i32 {
+        let mut dir = Direction::Up;
+
+        let mut x = start.0 as i32;
+        let mut y = start.1 as i32;
+
+        let mut visited = vec![vec![false; self.width]; self.height];
+        visited[x as usize][y as usize] = true;
+
+        loop {
+            if self.coords_to_index(x, y).is_some() {
+                let offset = dir.to_offset();
+                let new_x = x + offset.0;
+                let new_y = y + offset.1;
+
+                if let Some(next_index) = self.coords_to_index(new_x, new_y) {
+                    if self.arr[next_index] == '#' {
+                        dir = dir.turn_right();
+                    } else {
+                        x = new_x;
+                        y = new_y;
+
+                        visited[x as usize][y as usize] = true;
+                    }
+                } else {
+                    break;
+                }
+            }
+        }
+
+        visited[start.0][start.1] = false;
+
+        let res = visited
+            .iter()
+            .flatten()
+            .enumerate()
+            .filter_map(|(index, val)| {
+                let coords = self.index_to_coords(index);
+
+                if *val {
+                    Some(coords)
+                } else {
+                    None
+                }
+            })
+            .filter(|wall_coords| {
+                let wall_index = self
+                    .coords_to_index(wall_coords.0 as i32, wall_coords.1 as i32)
+                    .unwrap();
+
+                let mut x = start.0 as i32;
+                let mut y = start.1 as i32;
+                let mut dir = Direction::Up;
+
+                let mut visited =
+                    vec![vec![(false, Direction::default()); self.width]; self.height];
+                visited[x as usize][y as usize] = (true, dir);
+
+                loop {
+                    if self.coords_to_index(x, y).is_some() {
+                        let offset = dir.to_offset();
+                        let new_x = x + offset.0;
+                        let new_y = y + offset.1;
+
+                        if let Some(next_index) = self.coords_to_index(new_x, new_y) {
+                            let next_spot = visited[new_x as usize][new_y as usize];
+                            if next_spot.0 && next_spot.1 == dir {
+                                break true;
+                            }
+
+                            if self.arr[next_index] == '#' || wall_index == next_index {
+                                dir = dir.turn_right();
+                            } else {
+                                x = new_x;
+                                y = new_y;
+
+                                visited[x as usize][y as usize] = (true, dir);
+                            }
+                        } else {
+                            break false;
+                        }
+                    }
+                }
+            })
+            .collect::<Vec<_>>();
+
+        res.len() as i32
     }
 }
 
@@ -101,7 +194,11 @@ pub fn part_one(input: &str) -> Option<i32> {
 }
 
 pub fn part_two(input: &str) -> Option<i32> {
-    None
+    let mut map = Map::new(input);
+
+    let start = map.get_start();
+
+    Some(map.parse_cycle_paths(start))
 }
 
 #[cfg(test)]
@@ -118,6 +215,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&read_example(DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(6));
     }
 }
