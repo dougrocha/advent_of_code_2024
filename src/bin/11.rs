b086b3cff98b005b2fc150b3b1464e3ea9f9c041
Douglas Rocha <79188161+dougrocha@users.noreply.github.com>
2024-12-20 21:57:08 -0700

    completed day 11 part 2

--- src/bin/11.rs
+++ src/bin/11.rs
@@ -3,66 +3,81 @@ use std::collections::HashMap;
 advent_of_code_2024::solution!(11);
 
 struct Stones {
-    arr: Vec<u64>,
-    cache: HashMap<(u64, usize), u64>,
+    stones: HashMap<u64, usize>,
 }
+
 impl From<&str> for Stones {
     fn from(value: &str) -> Self {
         Self {
-            arr: value
-                .trim()
-                .split(" ")
-                .map(|x| x.parse::<u64>().unwrap())
-                .collect(),
-            cache: HashMap::default(),
+            stones: value
+                .split_whitespace()
+                .filter_map(|x| x.parse::<u64>().ok())
+                .fold(HashMap::new(), |mut acc, stone| {
+                    *acc.entry(stone).or_insert(0) += 1;
+                    acc
+                }),
         }
     }
 }
 
 impl Stones {
-    fn blink(&self, num: u64, blinks: usize) -> Vec<u64> {
-        let mut nums = vec![num];
+    fn blink(&mut self, blinks: usize) -> u64 {
+        (0..blinks).for_each(|_| {
+            self.stones
+                .clone()
+                .iter()
+                .filter(|(_, &x)| x != 0)
+                .for_each(|(&stone, &value)| {
+                    if stone == 0 {
+                        self.insert_cache(1, value);
+                        self.remove_cache(stone, value);
+                    } else if stone.to_string().len() % 2 == 0 {
+                        let str = stone.to_string();
+                        let (left, right) = str.split_at(str.len() / 2);
 
-        for _ in 0..blinks {
-            let mut current = Vec::new();
+                        self.insert_cache(right.parse::<u64>().unwrap(), value);
+                        self.insert_cache(left.parse::<u64>().unwrap(), value);
 
-            nums.iter().for_each(|num| {
-                if *num == 0 {
-                    current.push(1);
-                } else if num.to_string().len() % 2 == 0 {
-                    let str = num.to_string();
-                    let (left, right) = str.split_at(str.len() / 2);
+                        self.remove_cache(stone, value);
+                    } else {
+                        self.insert_cache(stone * 2024, value);
+                        self.remove_cache(stone, value);
+                    }
+                });
+        });
 
-                    current.push(left.parse::<u64>().unwrap());
-                    current.push(right.parse::<u64>().unwrap());
-                } else {
-                    current.push(*num * 2024);
-                }
-            });
-
-            nums = current;
-        }
-
-        nums
+        self.stones.values().map(|v| *v as u64).sum()
     }
 
-    fn blink_with_cache(&self) -> u64 {
-        0
+    fn insert_cache(&mut self, stone: u64, value: usize) {
+        if let Some(seen) = self.stones.get_mut(&stone) {
+            *seen += value;
+        } else {
+            self.stones.insert(stone, value);
+        }
+    }
+
+    fn remove_cache(&mut self, stone: u64, value: usize) {
+        if let Some(seen) = self.stones.get_mut(&stone) {
+            *seen -= value;
+
+            if *seen == 0 {
+                let _ = self.stones.remove(&stone);
+            }
+        }
     }
 }
 
 pub fn part_one(input: &str) -> Option<u64> {
-    let stones = Stones::from(input);
+    let mut stones = Stones::from(input);
 
-    let sum = stones.arr.iter().flat_map(|x| stones.blink(*x, 25)).count();
-
-    Some(sum as u64)
+    Some(stones.blink(25))
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    let stones = Stones::from(input);
+    let mut stones = Stones::from(input);
 
-    Some(0)
+    Some(stones.blink(75))
 }
 
 #[cfg(test)]
@@ -79,6 +94,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&read_example(DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(65_601_038_650_482));
     }
 }
