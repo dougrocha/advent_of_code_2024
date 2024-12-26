83bb5f3ba0ae68507d2a066612568689540d0a5c
Douglas Rocha <79188161+dougrocha@users.noreply.github.com>
2024-12-26 16:18:32 -0700

    added more math traits

--- src/math.rs
+++ src/math.rs
@@ -1,16 +1,16 @@
-#[derive(Default, Debug, Clone, Copy)]
+#[derive(Default, Debug, Clone, Copy, Hash, PartialEq, Eq)]
 pub struct IVec2 {
     pub x: i32,
     pub y: i32,
 }
 
-#[derive(Default, Debug, Clone, Copy)]
+#[derive(Default, Debug, Clone, Copy, Hash, PartialEq, Eq)]
 pub struct I64Vec2 {
     pub x: i64,
     pub y: i64,
 }
 
-#[derive(Default, Debug, Clone, Copy)]
+#[derive(Default, Debug, Clone, Copy, Hash, PartialEq, Eq)]
 pub struct UVec2 {
     pub x: u32,
     pub y: u32,
